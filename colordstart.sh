fb22ba04989665d892a19f27c96e64b067ed5601
usman-fazil <shakil@rnssol.com>
2019-10-25 14:39:35 +0500

    community members mapping

--- colordstart.sh
+++ colordstart.sh
@@ -1,7 +1,7 @@
+sudo rm -rf ~/.color*
 colord init usmanpc --chain-id=abc
 colorcli keys add validator
-colord add-genesis-account $(colorcli keys show validator -a) 10000000000color
+colord add-genesis-account $(colorcli keys show validator -a) 1000000000000uclr
 colord gentx --name validator
 colord collect-gentxs
 colord start
-
